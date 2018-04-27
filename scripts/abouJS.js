47ea76936ee287db492f5d6c6bbce7d2a92f5950
Sher Shah <sher.shah010@gmail.com>
2018-04-26 20:16:52 -0700

    hopeful this works

--- scripts/abouJS.js
+++ scripts/abouJS.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 							add_item("LANGUAGES", "Over the years, I have picked up many programming languages. Here is a list of them:", lang, "b"),
 							add_item("COMPETITIONS", "One hobby of mine is take part in compeitions. I find the challenges engaging and rewarding. Here are the compeitions I have been involved in:", comp, "c"),
 							add_item("CONTACT", "A list of ways you can contact me:", cont, "d")];
-	for (i = 0; i < items.length; i++){
+	for (var i = 0; i < items.length; i++){
 		$('body').append(items[i]);
 	}
 	$(".wrapper").addClass("load");
--- scripts/projJS.js
+++ scripts/projJS.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
   var items = [add_project("Scheme Text Editor", "This is an easy to use, text editor for the programing language, Scheme, which is a dialect of LISP. This was built using JavaFX, the RichTextFX API, and e(fx)clipse tooling and runtime. The program has the capability to open, save, and edit multiple Scheme files simultaneously. It is also provides color coding to make programming easier.", "img/editor.gif", 'a'),
               add_project("Interviews with Veterns", "A highschool project were I took interviews with veterns from World War Two, The Korean War, and the Vietnam War. I compiled the interviews into an interactive experiance were users can assume the role of the interviewer. Some features include a save state and multiple dialog options allowing for multiple playthrough. The tools I used were JavaFX and CSS.", "img/english.gif", 'b'),
               add_project("Text Based Adventure", "A silly, yet dark, text based adventure game. Similar to DUNNET on EMACS. I used C++ and Microsoft Visual Studios to create this project.", "img/text1.gif", 'c')];
-  for (i = 0; i < items.length; i++){
+  for (var i = 0; i < items.length; i++){
     $("body").append(items[i]);
   }
 	$(".wrapper").addClass("load");
