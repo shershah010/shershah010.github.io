6ee5fd641f13df6ea893b1cc583bd66286796f47
Sher Shah <sher.shah010@gmail.com>
2018-05-18 16:17:42 -0700

    fixed menu overflow error by changing overflow to overflow-y

--- scripts/portJS.js
+++ scripts/portJS.js
@@ -2,9 +2,9 @@ function menu(mu){
   $(mu).toggleClass("change");
   $(".nav").toggleClass("not_active");
   if ($(mu).hasClass("change")){
-    $("html").css({"margin" : "0", "height" : "100%", "overflow" : "hidden"});
+    $("html").css({"margin" : "0", "height" : "100%", "overflow-y" : "hidden"});
   } else {
-      $("html").css({"margin" : "auto", "height" : "auto", "overflow" : "scroll"});
+      $("html").css({"margin" : "auto", "height" : "auto", "overflow-y" : "scroll"});
   }
 }
 $(document).ready(function(){
--- scripts/projJS.js
+++ scripts/projJS.js
@@ -19,8 +19,8 @@ function menu(mu){
   $(mu).toggleClass("change");
   $(".nav").toggleClass("not_active");
   if ($(mu).hasClass("change")){
-    $("html").css({"margin" : "0", "height" : "100%", "overflow" : "hidden"});
+    $("html").css({"margin" : "0", "height" : "100%", "overflow-y" : "hidden"});
   } else {
-      $("html").css({"margin" : "auto", "height" : "auto", "overflow" : "scroll"});
+      $("html").css({"margin" : "auto", "height" : "auto", "overflow-y" : "scroll"});
   }
 }
