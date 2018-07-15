708d8c091e53d9c6dbf1367ffb785adc9da12f1f
Sher Shah <sher.shah010@gmail.com>
2018-07-14 17:01:49 -0700

    added css features

--- scripts/survival.js
+++ scripts/survival.js
@@ -1,7 +1,16 @@
 var timerLabel = document.querySelector(".timer");
+var urgent_message = document.querySelector(".urgent p");
+var daily_message = document.querySelector(".daily p");
 var time = 1;
 timerLabel.innerHTML = time + " hr";
 setInterval(function(){
 	time = time + 1;
 	timerLabel.innerHTML = time + " hrs";
 }, 30000);
+
+if (daily_message.innerHTML == ""){
+	document.querySelector(".daily").classList.add("hidden");
+}
+if (urgent_message.innerHTML == ""){
+	document.querySelector(".urgent").classList.add("hidden");
+}
