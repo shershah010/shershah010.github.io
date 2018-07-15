5e36ce237c4f61056992fa8effe2a4a00ec086cc
Sher Shah <sher.shah010@gmail.com>
2018-07-14 20:43:15 -0700

    more feautes

--- scripts/survival.js
+++ scripts/survival.js
@@ -3,9 +3,12 @@ var urgent_message = document.querySelector(".urgent p");
 var daily_message = document.querySelector(".daily p");
 var food_label = document.querySelector("li:nth-child(1)");
 var supplies_label = document.querySelector("li:nth-child(2)");
+var member_label = document.querySelector("li:nth-child(3)");
 var food = 5;
 var supplies = 5;
 var food_cost = 3;
+var member_food = 1;
+var member_supplies = 0;
 var nice_weather = {
 	"weather" : "It is a nice day.",
 	"food" : 2,
@@ -37,26 +40,27 @@ var weather = weathers[0];
 timerLabel.innerHTML = time + " hr";
 food_label.innerHTML = "Food: " + food;
 supplies_label.innerHTML = "Supplies: " + supplies;
+daily_message.innerHTML = weather["weather"];
+member_label.innerHTML = "Members (" + (member_food + member_supplies) + ")";
 setInterval(function(){
+	food_cost = 3 + member_food + member_supplies;
 	time = time + 1;
 	timerLabel.innerHTML = time + " hrs";
-	food += weather["food"];
-	supplies += weather["supplies"];
+	food += weather["food"] * member_food;
+	supplies += weather["supplies"] * member_supplies;
 	food_label.innerHTML = "Food: " + food;
 	supplies_label.innerHTML = "Supplies: " + supplies;
+	member_label.innerHTML = "Members (" + (member_food + member_supplies) + ")";
 	if (time % 24 == 0){
 		weather = getRandomWeather();
 		daily_message.innerHTML = weather["weather"];
 		food = food - food_cost;
 	}
+	if (Math.random() > 0.9){
+		urgent_message.innerHTML = "";
+	}
 }, 30000);
 
-if (daily_message.innerHTML == ""){
-	document.querySelector(".daily").classList.add("hidden");
-}
-if (urgent_message.innerHTML == ""){
-	document.querySelector(".urgent").classList.add("hidden");
-}
 function getRandomWeather() {
 	return weathers[Math.floor(Math.random() * weathers.length)];
 }
