e52fab03b2653d04abac71a05de2bc4d0698faa5
Sher Shah <sher.shah010@gmail.com>
2018-07-18 17:12:38 -0700

    js urgent features

--- scripts/survival.js
+++ scripts/survival.js
@@ -3,12 +3,13 @@ var urgent_message = document.querySelector(".urgent p");
 var daily_message = document.querySelector(".daily p");
 var food_label = document.querySelector("li:nth-child(1)");
 var supplies_label = document.querySelector("li:nth-child(2)");
-var member_label = document.querySelector("li:nth-child(3)");
+var member_label = document.querySelector("li:nth-child(4)");
 var food = 5;
 var supplies = 5;
 var food_cost = 3;
 var member_food = 1;
 var member_supplies = 0;
+var urgent_list = ["A wandering traveler has arrived. Do you let them in?"];
 var nice_weather = {
 	"weather" : "It is a nice day.",
 	"food" : 2,
@@ -42,6 +43,13 @@ food_label.innerHTML = "Food: " + food;
 supplies_label.innerHTML = "Supplies: " + supplies;
 daily_message.innerHTML = weather["weather"];
 member_label.innerHTML = "Members (" + (member_food + member_supplies) + ")";
+function add_detection(id){
+	switch(id){
+	case 1:
+		break;
+	}
+}
+var urgent_fn;
 setInterval(function(){
 	food_cost = 3 + member_food + member_supplies;
 	time = time + 1;
@@ -56,7 +64,13 @@ setInterval(function(){
 		daily_message.innerHTML = weather["weather"];
 		food = food - food_cost;
 	}
-	if (Math.random() > 0.9){
+	if (Math.random() > 0.5){
+		document.querySelector(".urgent").classList.remove("hidden");
+		var rand = Math.floor(Math.random()*urgent_list.length);
+		urgent_message.innerHTML = urgent_list[rand];
+		urgent_fn = function(){add_detection(rand); };
+	} else {
+		document.querySelector(".urgent").classList.add("hidden");
 		urgent_message.innerHTML = "";
 	}
 }, 30000);
@@ -64,3 +78,13 @@ setInterval(function(){
 function getRandomWeather() {
 	return weathers[Math.floor(Math.random() * weathers.length)];
 }
+function reject(){
+	document.querySelector(".urgent").classList.add("hidden");
+	urgent_message.innerHTML = "";
+}
+function confirm(){
+	urgent_fn();
+}
+
+document.querySelector("button:nth-child(1)").addEventListener("click", confirm);
+document.querySelector("button:nth-child(2)").addEventListener("click", reject);
