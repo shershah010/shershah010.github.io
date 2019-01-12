004d45c2d9449c99a877209f11cff9582536f377
Sher Shah <sher.shah010@gmail.com>
2019-01-12 11:50:53 -0800

    added three.js and images

---
+++ operations/U87ZA/main.js
@@ -0,0 +1,12 @@
+// var tabs = document.querySelectorAll(".tab");
+//
+// for (let i = 0; i < tabs.length; i += 1) {
+//   tabs[i].addEventListener("click", function(e) {
+//     if (this.classList.contains("fullscreen")) {
+//       this.classList.remove("fullscreen");
+//     } else {
+//       this.classList.add("fullscreen");
+//       this.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+//     }
+//   });
+// }
